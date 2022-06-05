19eb2140ab0b1572b1d6dffd8753d9cedb257de0
gardonyim <mark.gardonyi@gmail.com>
2022-06-05 16:30:59 +0200

    feat(movie recommendation app): init project

---
+++ masterwork/src/test/java/com/example/masterwork/MasterworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masterwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
