dfe352fd8c50ab36f763449c8bb6608cdadff213
gardonyim <mark.gardonyi@gmail.com>
2022-06-17 12:20:48 +0200

    feat(docker, swagger): add Dockerfile and settings for swagger

--- masterwork/src/main/java/com/example/masterwork/MasterworkApplication.java
+++ masterwork/src/main/java/com/example/masterwork/MasterworkApplication.java
@@ -2,12 +2,24 @@ package com.example.masterwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+@EnableWebMvc
 @SpringBootApplication
 public class MasterworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasterworkApplication.class, args);
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.masterwork")).build();
 	}
 
 }
